001f07813c5d4fd0b12f648f63bcf0545eb8337d
joninoni <jonibeltranhayer@gmail.com>
2024-01-28 11:30:46 -0600

    solo puse comentarios para recordar

--- js/api.js
+++ js/api.js
@@ -2,15 +2,15 @@ const url=`http://localhost:4000/clientes`;
 export const nuevoCliente= async cliente => {
     try {
        await fetch(url,{
-            method:"POST",
-            body:JSON.stringify(cliente),
+            method:"POST",//indicamos el metodo a usar
+            body:JSON.stringify(cliente),//la informacion que vamos a usar
             headers:{
-                "Content-Type":"application/json"
+                "Content-Type":"application/json"//el tipo de datos que estamos enviando
             }
         })
 
         window.location.href="index.html";
-        
+
     } catch (error) {
         console.log(error);
     }
