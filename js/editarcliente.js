e5b704c9248e4ef3a0c656407ffa96e324bbb204
joninoni <jonibeltranhayer@gmail.com>
2024-01-29 12:20:02 -0600

    llenando los campos con la informacion del cliente

--- js/editarcliente.js
+++ js/editarcliente.js
@@ -1,6 +1,10 @@
 import {obtenerCliente} from "./api.js";
 //variables
-
+const nombreInput=document.querySelector("#nombre");
+const emailInput=document.querySelector("#email");
+const empresaInput=document.querySelector("#empresa");
+const telefonoInput=document.querySelector("#telefono");
+const idInput=document.querySelector("#id");
 //eventos
     document.addEventListener("DOMContentLoaded",obtenerIDCliente);
 
@@ -10,6 +14,15 @@ async function obtenerIDCliente(){
     const idCliente = parametrosUrl.get("id");//obtiene el id del cliente
     const cliente = await obtenerCliente(idCliente);
 
-    console.log(cliente);
+    mostrarCliente(cliente);
 }
 
+function mostrarCliente(cliente){
+    const {nombre,email,empresa,telefono,id} = cliente;
+    //llenando los campos con los valores del cliente
+    nombreInput.value=nombre;
+    emailInput.value=email;
+    empresaInput.value=empresa;
+    telefonoInput.value=telefono;
+    idInput.value=id;
+}
